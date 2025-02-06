44b2e96b814a8c554068c8eeb44563c54a8efcb1
Paula <paula.farid9@gmail.com>
2025-02-06 23:11:52 +0200

    first

--- src/main/java/ReverseWordInPlace.java
+++ src/main/java/ReverseWordInPlace.java
@@ -2,7 +2,6 @@
 public class ReverseWordInPlace {
 
     public static void main(String[] args) {
-
         char[] input = "i love programming very much".toCharArray();
         ReverseWords_Range.reverseWords(input);
         System.out.println(new String(input));
--- src/main/java/SwapBetweenCharacters.java
+++ src/main/java/SwapBetweenCharacters.java
@@ -1,11 +1,9 @@
+
 public class SwapBetweenCharacters {
 
     public static void swapCharacters(char[] characters, int firstIndex, int secondIndex) {
-
         characters[firstIndex] ^= characters[secondIndex];
         characters[secondIndex] ^= characters[firstIndex];
         characters[firstIndex] ^= characters[secondIndex];
-
     }
-
 }
